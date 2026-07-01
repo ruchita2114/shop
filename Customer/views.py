516ee1c9c706edb050f04f2753a58fb093d34db6
Ruchita <ruchitagururaj@gmail.com>
2026-07-01 13:20:58 +0530

    test home view

--- Customer/views.py
+++ Customer/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html")
+    return HttpResponse("Home is working")
 
 def product(request):
     return render(request, "customer/product.html")
