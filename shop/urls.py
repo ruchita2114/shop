6ee7cb2f46765f37e4dd7181b225c74a9dfbc9b9
Ruchita <ruchitagururaj@gmail.com>
2026-07-01 21:25:39 +0530

    navbar fix

--- shop/urls.py
+++ shop/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     
     path("admin/", admin.site.urls),
-    path("" , include("Customer.urls")),
+    path("", include("Customer.urls")),
    
 ]
